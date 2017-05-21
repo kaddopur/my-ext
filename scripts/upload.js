19589c2bcb7840f4d3c55140abb0a24164a66bb4
Jason Huang <cjhuang@yahoo-inc.com>
2017-05-21 21:18:04 +0800

    update travis setting

--- scripts/upload.js
+++ scripts/upload.js
@@ -3,14 +3,14 @@ const uploadOptions = {
     accounts: {
         default: {
             publish: true,
-            client_id: CLIENT_ID,
-            client_secret: CLIENT_SECRET,
-            refresh_token: REFRESH_TOKEN
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            refresh_token: process.env.REFRESH_TOKEN
         }
     },
     extensions: {
         my_ext: {
-            appID: APP_ID,
+            appID: process.env.APP_ID,
             zip: `${process.cwd()}/bundle.zip`
         }
     },
